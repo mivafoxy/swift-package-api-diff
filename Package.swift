74fe94f8f3468068d256fb7aeee21bd996fc5451
mivafox <creatox1996@gmail.com>
2024-01-24 21:07:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftPackageApiDiff",
-            targets: ["swift-package-api-diff"]
+            targets: ["SwiftPackageApiDiff"]
         )
     ],
     dependencies: [
